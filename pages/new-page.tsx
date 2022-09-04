f450391cc00681911e2556bcf03eede9e2f41eaa
ShingoTennichi <sngsng.ttt@gmail.com>
2022-09-04 00:36:59 -0700

    add1

---
+++ pages/new-page.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+
+const NewPage: NextPage = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <p>add 1</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default NewPage;
